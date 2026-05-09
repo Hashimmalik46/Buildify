b22d149cbf997bf3372152a81e7cb8d94870f172
ZahranDhar <zahrannazirdhar@gmail.com>
2026-05-09 16:05:44 +0530

    routes

--- server/routes/EmployeeEvaluation.py
+++ server/routes/EmployeeEvaluation.py
@@ -3,9 +3,15 @@ from flask import request, jsonify
 
 def EmployeeEvaluation(app):
 
+    #signup/skillchange
     @app.route('/employee-evaluation', methods=['POST'])
     def employee_evaluation():
         employee_id = request.json.get('employee_id')
         evaluation = evaluateEmployee(employee_id)
+        #db
         return jsonify(evaluation)
+    
+    #route....employeesroute....hr on prompt change
+    #callgenerateskillsfunction
+    #db
     
